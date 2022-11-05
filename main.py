3e1f705e381bff06088845e9124994229e42367b
Ralph <ralph_souza@hotmail.com>
2022-11-04 21:23:44 -0300

    Testing regex to compare inputed string with a pre defined pattern

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
+import re
+
+
+def user_name():
+    pattern = r"[A-Za-z\d]"
+    while True:
+        name = input("Please enter your name: ")
+        if len(name) <= 3 and str(re.match(pattern, name)):
+            print("Please enter a valid name: ")
+        else:
+            print(f"Welcome {name}!")
+            return name
+
+
+def show_menu():
+    user_name()
+    print(f"""
+    *********************************
+    It`s time to play some Blackjack!
+    *********************************
+    """)
+
+
 def blackjack():
     pass
+
+
+show_menu()
